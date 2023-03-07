64591af6a8757c6abc79016892ba241e6e5243bf
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-03-07 16:53:56 +0100

    add about-us page

---
+++ src/pages/AboutUs.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name:'AboutUS',
+    components:{
+
+    },
+    data() {
+        return {
+            urlAddress: 'http://127.0.0.1:8000/api/about-us',
+        }
+    },
+    methods: {
+    
+    },
+    created() {
+
+    },
+}
+</script>
+
+<template lang="">
+    <div class="row">
+        <div class="col-12">
+            <h2>
+                About Us:
+            </h2>
+        </div>
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
